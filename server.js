ef419622696c24d6b0634ab41a74909f06a339e8
- <s1074@ms.ds.uhc.com>
2019-01-18 23:50:34 +0530

    setup start script and heroku port

--- server.js
+++ server.js
@@ -7,6 +7,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const port = process.env.PORT || 3000;
 
 var app = express();
 
@@ -64,6 +65,6 @@ app.get('/bad', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('server is up and running on port 3000');
+app.listen(port, () => {
+  console.log(`server is up and running on port: ${port}`);
 });
